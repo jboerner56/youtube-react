b82bff4c792505d2399cbe123f8a676ec331992d
Jason Boerner <jboerner56@gamil.com>
2019-05-13 10:05:39 -0400

    fixes bug with selecting video

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import YoutubeSearch from 'youtube-api-search';
 import SearchBar from './components/SearchBar';
 import VideoList from './components/VideoList';
 import VideoDetail from './components/VideoDetail';
-// import YoutubeApi from './YoutubeApi';
 const ApiKey = 'AIzaSyBucCifjcZm6dGx1Lt0nyF0X9t07X-w_S0';
 class App extends React.Component {
   constructor(props){
@@ -19,14 +18,17 @@ class App extends React.Component {
     return (
 
       <div className="App">
-      <div className="search-bar">
-        <SearchBar handleFormSubmit={this.handleSubmit}/>
-      </div>
-      <div className="video-detail">
-      <VideoDetail/>
-      </div>
+        <div className="search-bar">
+          <SearchBar handleSearchSubmit={s => this.videoSearch(s)}/>
+        </div>
+        <div className="video-detail">
+          <VideoDetail video={this.state.selectedVideo}/>
+        </div>
       <div className="video-list">
-        <VideoList videos={this.state.videos}/>
+        <VideoList videos={this.state.videos} 
+        handleVideoSelect={selected => this.setState({
+          selectedVideo: selected 
+        })}/>
       </div>
       </div>
     )
--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,11 +1,11 @@
 import React from 'react';
 const VideoItem = (props) => {
     const video = props.video;
-    const userSelected = props.userSelected;
+    const handleVideoSelect = props.handleVideoSelect;
     const videoThumbnail = video.snippet.thumbnails.default.url;
     return (
         <div>
-            <li onClick={() => userSelected(video)} className="video-item">
+            <li onClick={() => handleVideoSelect(video)} className="video-item">
                 <img src={videoThumbnail} alt=""/>
                 <div className="video-title">{video.snippet.title}</div>
             </li>
